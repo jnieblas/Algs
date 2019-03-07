f9d155f2e022dcb8f6e43c23f31c8eaff916ff6a
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-03-07 00:38:55 -0700

    implemented routes for karatsuba's alg

---
+++ routes/karatsuba.js
@@ -0,0 +1,17 @@
+// karatsuba.js - Karatsuba's Alg route module
+
+var express = require('express');
+var router = express.Router();
+
+// Home page route
+router.get('/', function(req, res) {
+    res.send('Karatsuba\'s Algorithm');
+});
+
+
+// Efficiency analysis & details route
+router.get('/details', function (req, res) {
+    res.send('About Karatsuba\'s Algorithm');
+});
+
+module.exports = router;
