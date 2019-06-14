ef9798458419feefd06c4727257d1f659e47a9ca
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-06-14 12:55:27 -0600

    Setup for use of Sass

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const index_controller = require('../controllers/indexController');
+
 // GET home page
 // Redirect index to a custom index location
-router.get('/', function(req, res) {
-  res.redirect('/algorithms');
-});
+router.get('/', index_controller.index);
 
 module.exports = router;
